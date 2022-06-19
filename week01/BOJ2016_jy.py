e713236c85da98516792a3752c2eb8c4a87ffa2c
Juyoung4 <47167335+Juyoung4@users.noreply.github.com>
2022-06-19 23:05:38 +0900

    [Solve] ✍ #BOJ 2016.미팅주선하기

--- week01/BOJ2016_jy.py
+++ week01/BOJ2016_jy.py
@@ -1,5 +1,8 @@
+from itertools import permutations
 
 T = int(input())
+
+CheckOrder = -1
 
 def CompareLove(loveMW, checkCoupleM, checkCarW, checkStack, i):
     if not checkCoupleM[loveMW[i-1][checkCarW[i]]] : # 아무도 선택한 사람 x면
@@ -16,9 +19,8 @@ def CompareLove(loveMW, checkCoupleM, checkCarW, checkStack, i):
                 nowIdx = j
         print("idx 확ㅇ니 : ", alreadyIdx, nowIdx)
         if alreadyIdx < nowIdx:
-            checkStack.insert(0, now)
+            checkStack.append(now)
             checkCarW[now] += 1 # 차인 여자의 인덱스 up -> 다음 남자 고를 수 있게
-            checkCoupleM[manNum+1] = already # 남자 짝궁 선정
         else:
             checkStack.insert(0, already)
             checkCarW[already] += 1 # 차인 여자의 인덱스 up -> 다음 남자 고를 수 있게
@@ -27,22 +29,30 @@ def CompareLove(loveMW, checkCoupleM, checkCarW, checkStack, i):
     return checkCoupleM, checkCarW, checkStack
 
 def makeCouple(loveMW, checkCoupleM, checkCarW, checkStack):
-    print("start!!")
-    print("첫번째 미팅")
+    global CheckOrder
+    print("###########start###########")
+    print("first 선호도 : ", loveMW)
     for i in range(5, 10): # 여자-남자 선호도
         checkCoupleM, checkCarW, checkStack = CompareLove(loveMW, checkCoupleM, checkCarW, checkStack, i+1)
-        print("첫번째 미팅 결과 : ", checkCoupleM, checkCarW, checkStack)
-    print("첫번째 끝나고 차인 사람 : ", checkStack)
-    print(" 차인사람 미팅 이어서-->")
+    print("first 미팅 결과 : ", checkCoupleM, checkCarW, checkStack)
+    print("first 끝나고 차인 사람 : ", checkStack)
+    
+    print("########차인사람 미팅 이어서-->#########")
     while checkStack:
-        CarWNum = checkStack.pop()
+        CarWNum = checkStack.pop(-1)
         checkCoupleM, checkCarW, checkStack = CompareLove(loveMW, checkCoupleM, checkCarW, checkStack, CarWNum)
         print(" 차인사람 (",CarWNum, ") 미팅 결과 : ", checkCoupleM, checkCarW, checkStack)
-
-    return 1
+        print("################다음미팅시작##########")
+    
+    if CheckOrder == -1 : 
+        CheckOrder = checkCoupleM[1]
+        return 1
+    elif CheckOrder > CheckOrder: 
+        return 0
+    
 
 for i in range(T):
-    loveMW = []
+    loveMW = [[0, 0, 0, 0, 0]]
 
     checkCoupleM = {
         1 : 0,
@@ -64,12 +74,20 @@ for i in range(T):
     for j in range(9):
         loveMW.append(list(map(int, input().split())))
 
-    loveMW.insert(0, [6, 7, 8, 9, 10]) # 태현
-    # 태현의 선호도들을 모두 만들어 for문 돌리기
-    print("man : ", loveMW)
+    for permu in permutations([6, 7, 8, 9, 10], 5):
+        loveMW[0] = list(permu)
 
-    makeCouple(loveMW, checkCoupleM, checkCarW, checkStack)
+        
+        # 태현의 선호도들을 모두 만들어 for문 돌리기
+        #print("man : ", loveMW)
+
+        if not makeCouple(loveMW, checkCoupleM, checkCarW, checkStack):
+            break
+            print("NO")
+    else:
+        print("YES")
 
     print()
+
     break
 
