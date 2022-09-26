81acb8a5c093df84e610c1007b8e9031875b9220
YEZZJII <65303837+suhyeji@users.noreply.github.com>
2022-09-26 21:29:03 +0900

    [Solve] ✍ #Programmmers 두 큐 합 같게 만들기

--- week12/Programmers118667_YJ.py
+++ week12/Programmers118667_YJ.py
@@ -4,7 +4,7 @@ def solution(queue1, queue2):
     queue1, queue2 = deque(queue1), deque(queue2)
     q1Sum, q2Sum = sum(queue1), sum(queue2)
 
-    for i in range(len(queue1) * 4): # 두 큐의 모든 원소를 교환하고 다시 복구
+    for i in range(len(queue1) * 4): # 두 큐의 모든 원소를 교환하고 다시 복구 
         if q1Sum == q2Sum:
             return i
 
